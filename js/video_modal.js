a0ebe15b422e131fff710677eeb896106488514d
Tomas Barber <43525620+tomineitor@users.noreply.github.com>
2021-06-29 17:20:31 -0400

    Se hicieron varias mejoras a nivel general.
    Se quitaron memes, se agregaron botones, además de que los videos ahora funcionan como un modal (ya no abre otra pestaña!)

---
+++ js/video_modal.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    // Gets the video src from the data-src on each button
+    var $videoSrc;
+    $(".botoncito2").click(function() {
+      $videoSrc = $(this).attr("data-src");
+      console.log("button clicked" + $videoSrc);
+    });
+  
+    // when the modal is opened autoplay it
+    $("#VideoModal").on("shown.bs.modal", function(e) {
+      console.log("modal opened" + $videoSrc);
+      // set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
+      $("#video").attr(
+        "src",
+        $videoSrc + "?autoplay=1&showinfo=0&modestbranding=1&rel=0&"
+      );
+    });
+  
+    // stop playing the youtube video when I close the modal
+    $("#VideoModal").on("hide.bs.modal", function(e) {
+      // a poor man's stop video
+      $("#video").attr("src", $videoSrc);
+    });
+  
+    // document ready
+  });
